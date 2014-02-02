01bb4fbe3a2e3576ef211fb6419a0fbd7fe1ef8d
Jeff Hui <jeff@jeffhui.net>
2014-02-02 02:51:41 -0800

    Initial Commit

---
+++ Crane/Crane.h
@@ -0,0 +1,13 @@
+//
+//  Crane.h
+//  Crane
+//
+//  Created by Jeff Hui on 2/2/14.
+//  Copyright (c) 2014 Jeff Hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Crane : NSObject
+
+@end
---
+++ Crane/Crane.m
@@ -0,0 +1,13 @@
+//
+//  Crane.m
+//  Crane
+//
+//  Created by Jeff Hui on 2/2/14.
+//  Copyright (c) 2014 Jeff Hui. All rights reserved.
+//
+
+#import "Crane.h"
+
+@implementation Crane
+
+@end
